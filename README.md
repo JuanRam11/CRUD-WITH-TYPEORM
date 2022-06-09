65556e3a8f025a6153ba18a1ef26b68042d042f5
Juan Ramon Then <juanramon@hotmail.com>
2022-06-09 11:44:03 -0400

    Added more logic to allow the user to input firstname, lastname and age

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 Steps to run this project:
 
 1. Run `npm i` command
-2. Setup database settings inside `data-source.ts` file
 
 # After running steps adobe
 
-1. go to data-source file and replace with your own database settings
-2. change synchronize to true on data-source file
-3. comment out dataFind, dataDelete and updateDate from index.ts file
-4. run `npm start` command
-5. with begin done is going to synchronize database creating table with all columns
-6. then you should commnet out dataInsert, dataDelete, dataUpdate
-7. After that you should see what is on the table from the console
+1. setup database settings inside `data-source.ts` file
+2. change synchronize to true on `data-source.ts` file
+3. run `npm start` command
+4. with begin done is going to synchronize database creating table with all columns
+5. After that you should see the table with all columns from MYSQL database
+6. go to POSTMAN and with http://localhost:3000/user you can do all CRUD
+7. GET users (GET), http://localhost:3000/user will response with all users in json format
+8. GET specific user (GET), http://localhost:3000/user will response with a specific user in json format
+9. INSERT (POST), http://localhost:3000/user will response with rows that were inserted in json format
+10. UPDATE (PUT), http://localhost:3000/user:id will response with rows that were updated in json format
+11. DELETE (DELETE), http://localhost:3000/user:id will response with that were deleted
--- src/index.ts
+++ src/index.ts
@@ -41,37 +41,56 @@ AppDataSource.initialize()
     // start express server
     app.listen(3000);
 
-    //To Search specific colums
+    //TO SEARCH ALL USERS //To Search specific colums
     app.get('/user', async (req: Request, res: Response) => {
-      const dataFind = await AppDataSource.manager.find(User, {
+      const dataFind = await AppDataSource.manager.find(
+        User /* , {
         select: { firstName: true, id: true },
-        where: { age: 0, lastName: 'Saw' },
+     /*    where: { age: 0, lastName: 'Saw' }, 
+      } */
+      );
+      res.json(dataFind);
+    });
+
+    //To find specific users using id on the URL
+    app.get('/user:id', async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const dataFind = await AppDataSource.manager.find(User, {
+        /*         select: { firstName: true, id: true }, */
+        where: { id: id },
       });
       res.json(dataFind);
     });
 
     // To insert data into table
     app.post('/user', async (req: Request, res: Response) => {
+      const firstName = req.body.firstName;
+      const lastName = req.body.lastName;
+      const age = req.body.age;
       const dataInsert = await AppDataSource.manager.insert(User, {
-        firstName: 'Prueba4',
-        lastName: 'Martin',
-        age: 65,
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
       });
       res.json({ affetedRows: dataInsert.raw.affectedRows });
     });
+
     // DELETE registry from database
-    app.delete('/user', async (req: Request, res: Response) => {
-      const dataDelete = await AppDataSource.manager.delete(User, { id: '17' });
+    app.delete('/user:id', async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const dataDelete = await AppDataSource.manager.delete(User, { id: id });
       res.json({ Delete: dataDelete });
     });
 
     // To update
-    app.put('/user', async (req: Request, res: Response) => {
+    app.put('/user:id', async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const firstName = req.params.firstName;
       const updateData = await AppDataSource.manager
         .createQueryBuilder()
         .update(User)
-        .set({ firstName: 'Prueba4' })
-        .where('id=:id', { id: 5 })
+        .set({ firstName: firstName })
+        .where('id=:id', { id: id })
         .execute();
       res.json({ affected: updateData });
     });
